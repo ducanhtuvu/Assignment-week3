e5d52dd340a59669bf5d5c125948b051a97b6d77
ducanhtuvu <ducanhtuvu@gmail.com>
2018-12-20 23:25:09 +0700

    - setup bootstrap

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const debug = require('debug')('app');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+
+const adminRouter = require('./routes/admin');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +44,17 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.start = (PORT, MONGO_URL) => {
+	mongoose
+		.connect(MONGO_URL)
+		.then(() => {
+			debug(MONGO_URL + ' database connect success');
+			app.listen(PORT, () => console.log('App started and listening on port', PORT));
+		})
+		.catch(err => {
+			debug('Database connection error:' + err);
+		});
+};
 
 module.exports = app;
