509d4d474a49bbd115f49872120a43bc930f7b71
ducanhtuvu <ducanhtuvu@gmail.com>
2018-12-22 14:44:20 +0700

    - add model file

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const Product = require('../models/Product')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('products', { title: 'Products' });
-});
+function wrapJson(body) {
+	if (body instanceof Error) {
+		return {
+			header: {
+				status: 400,
+				errorMessage: `${body.name}: ${body.message}`,
+				currentDate: new Date(),
+			},
+			body,
+		}
+	}
+
+	return {
+		header: {
+			status: 200,
+			errorMessage: '',
+			currentDate: new Date(),
+			count: Array.isArray(body) ? body.length : body ? 1 : 0,
+		},
+		body,
+	}
+}
+
+// a middleware to enhance res object
+router.use((req, res, next) => {
+	// attach a new method `sendRest` to res object for later use
+	res.sendRest = body => {
+		if (body instanceof Error) {
+			res.statusCode = 400
+		}
+		return res.json(wrapJson(body))
+	}
+	next()
+})
+
+
+// GET /api/products
+router.get(`/`, (req, res) => {
+	Product.find({})
+		.exec()
+		.then(products => {
+			res.sendRest(products)
+		})
+		.catch(err => {
+			res.sendRest(err)
+		})
+})
 
 module.exports = router;
